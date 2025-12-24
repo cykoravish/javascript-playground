367266e1863363b51f8da9ef78b4d936bbed9592
cykoravish <ravishbisht85@gmail.com>
2025-12-24 07:41:07 +0530

    day 10 question add

--- 15 days js questions/.js
+++ 15 days js questions/.js
@@ -124,3 +124,10 @@ arr.forEach((e) => {
 });
 console.log(newArr);
 */
+
+/*
+//10. Given a nested array, flatten it into a single-level array
+let arr = [1, 2, [3, 4, [5, 6]], [7, 8], [9], 10];
+let newArr = arr.flat(Infinity);
+console.log(newArr);
+*/
